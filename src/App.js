07d38f39ccc60223fda1e3c9feab69896678e51b
Gano603 <khawar603@gmail.com>
2023-08-02 14:03:03 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.scss'
-import {BrowserRouter as BR, Route, Routes} from 'react-router-dom';
+// import {BrowserRouter as BR, Route, Routes} from 'react-router-dom';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 
 
 
 function App() {
-  return (
-    <BR>
+  return (<>
+    {/*  <BR> */}
     <Header />
-    <Routes>
-    <Route path="/" element={<Home />} />
-    </Routes>
-    </BR>
+    {/*  <Routes> */}
+    {/*  <Route path="/" element={<Home />} /> */}
+    <Home />
+    {/*  </Routes> */}
+    {/*  </BR> */}
+    </>
   );
 }
 
